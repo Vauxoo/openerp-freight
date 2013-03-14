37e6a19c9b7d81ee7562c3bddc238d946b8fb499
OscarAlca <oszckar@gmail.com>
2013-03-14 01:06:40 -0600

    [ADD] basic structure for multi responsibles on a vehicle feature

---
+++ fleet_vehicle_responsibles/__openerp__.py
@@ -0,0 +1,45 @@
+#-*- coding:utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution    
+#    Copyright (C)2010-  OpenERP SA (<http://openerp.com>). All Rights Reserved
+#    oszckar@gmail.com
+#
+#    Developed by Oscar Alcala <oszckar@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name" : "Fleet Vehicle Responsibles",
+    "version" : "0.1",
+    "depends" : ["fleet",],
+    "author" : "Vauxoo",
+    "description" : """
+    This module provides the capability to add multiple responsibles to a vehicle
+    TODO: add the location linked to sub-zones or sub-companies  
+    
+                    """,
+    "website" : "http://vauxoo.com",
+    "category" : "Managing vehicles and contracts",
+    "init_xml" : [
+    ],
+    "demo_xml" : [
+    ],
+    "update_xml" : [
+        "fleet_vehicle_responsibles_view.xml",
+    ],
+    "active": False,
+    "installable": True,
+}
