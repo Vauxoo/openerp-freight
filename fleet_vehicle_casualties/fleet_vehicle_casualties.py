a711ae7ea1a0373a2480d812923dede2f66559cd
OscarAlca <oszckar@gmail.com>
2013-03-14 12:40:47 -0600

    [ADD] Basic structure for the casualties module added

---
+++ fleet_vehicle_casualties/fleet_vehicle_casualties.py
@@ -0,0 +1,44 @@
+#-*- coding:utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution    
+#    Copyright (C)2010-  OpenERP SA (<http://openerp.com>). All Rights Reserved
+#    oszckar@gmail.com
+#
+#    Developed by Oscar Alcala <oszckar@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp import pooler, tools
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+
+class fleet_vehicle_casualties(osv.osv):
+    """
+    OpenERP Model : fleet_vehicle_casualties
+    """
+    
+    _name = 'fleet.vehicle.casualties'
+    _columns = {
+        'name':fields.char('Casualty', size=64, required=False, readonly=False),
+    }
+fleet_vehicle_casualties()
+
+class fleet_vehicle(osv.osv):    
+    _inherit = 'fleet.vehicle'
+    _columns = {
+        'casualties_ids':fields.one2many('fleet.vehicle.casualties', 'casualty_id', 'Casualties', required=False),
+    }
+fleet_vehicle()
