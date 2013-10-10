9a1fcdeff0ab670f8c981cdb21e2d85a76e84833
Yanina Aular <yanina.aular@vauxoo.com>
2013-10-10 11:33:00 -0430

    [ADD] New module to control the areas to ship orders

---
+++ fleet_zone/model/fleet_zone.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+###############################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP Venezuela (<http://openerp.com.ve>).
+#    All Rights Reserved
+# Credits######################################################
+#    Coded by: Yanina Aular <yanina.aular@vauxoo.com>
+#    Planified by: Humberto Arocha <humbertoarocha@gmail.com>
+#    Audited by: Nhomar Hernandez <nhomar@gmail.com>
+###############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
+
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+from openerp import tools
+import datetime
+    
+class fleet_zone(osv.Model):
+    _name = 'fleet.zone'
+    _description = 'Zone'
+    _columns = {
+        'name' : fields.char('Zone Name', 256, help='Zone Name'),
+        'benchmark_ids' : fields.one2many('fleet.benchmark', 'zone_id', 'Benchmark', help=''),
+        'sequence': fields.integer('Sequence', help="Gives the sequence of this benchmark when displaying the zone."),   
+    }
+
+class fleet_benchmark(osv.Model):
+    _name = "fleet.benchmark"
+    _description = "Zone Benchmark"
+    _columns = {
+        "name" : fields.char("Zone Benchmark", 256, help="Zone Benchmark"),
+        "zone_id" : fields.many2one("fleet.zone", "Zone", help=""),
+        
+    }
