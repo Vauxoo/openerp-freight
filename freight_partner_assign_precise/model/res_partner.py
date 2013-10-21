b36e21ae43939e98350b04f8299b7be95b7407c7
Yanina Aular <yanina.aular@vauxoo.com>
2013-10-21 10:27:35 -0430

    [ADD] method to geolocate partner address with precision

--- freight_partner_assign_precise/model/res_partner.py
+++ freight_partner_assign_precise/model/res_partner.py
@@ -28,6 +28,13 @@ from openerp.osv import fields
 from openerp.tools.translate import _
 from openerp import tools
 import openerp.addons.decimal_precision as dp
+import logging
+_logger = logging.getLogger(__name__)
+from openerp.addons.crm_partner_assign import crm_partner_assign as CRM
+try:
+    from googlemaps import GoogleMaps
+except:
+    _logger.error('Install googlemaps "sudo pip install googlemaps" to use web_gmaps module.')
 
 class res_partner(osv.osv):
     _inherit = "res.partner"
@@ -38,3 +45,29 @@ class res_partner(osv.osv):
         'gmaps_lon': fields.float('Geo Longitude',
             digits_compute=dp.get_precision('Gmaps Partner')),
     }
+    
+    def precise_geo_localize(self, cr, uid, ids, context=None):
+        # Don't pass context to browse()! We need country names in english below
+
+        #gmaps = GoogleMaps("http://maps.googleapis.com/maps/api/js?key=AIzaSyBwNE-vFDyyOb62ODaRiqpiL2kz8wR0aTc")
+        for partner in self.browse(cr,uid,ids):
+
+            street = partner.street and partner.street or ''
+            street2 = partner.street2 and partner.street2 or ''
+
+            address = CRM.geo_query_address(
+                street="%s %s" % (street, street2),
+                zip=partner.zip,
+                city=partner.city,
+                state=partner.state_id.name,
+                country=partner.country_id.name)
+
+            #lat, lng = gmaps.address_to_latlng(address)
+            result = CRM.geo_find(address)
+  
+            if result:
+                self.write(cr, uid, [partner.id], {
+                    'gmaps_lat': result[0],
+                    'gmaps_lon': result[1],
+                }, context=context)
+        return True
